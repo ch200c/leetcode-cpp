f4f63bc5ca4c1d22a06be06561b0f1a55863884a
ch200c <an.dris.developer@gmail.com>
2020-03-11 22:01:33 +0000

    problem 1

--- src/1/memory_usage.cpp
+++ src/1/memory_usage.cpp
@@ -1,5 +1,6 @@
-#pragma once
-#include <vector> // std::vector
+#include "catch2/catch.hpp"
+#include <algorithm> // std::find
+#include <vector>    // std::vector
 
 namespace problem_1_memory {
 class Solution {
@@ -23,5 +24,12 @@ public:
     return std::vector<int>{};
   }
 };
+} // namespace problem_1_memory
 
-}; // namespace problem_1_memory
+TEST_CASE("Problem 1 (Memory Usage)") {
+  problem_1_memory::Solution solution;
+  auto result{solution.twoSum(std::vector<int>{2, 7, 11, 15}, 9)};
+
+  REQUIRE(std::find(result.cbegin(), result.cend(), 0) != result.cend());
+  REQUIRE(std::find(result.cbegin(), result.cend(), 1) != result.cend());
+}
